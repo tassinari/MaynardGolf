206aee8823667df19933441570f447dca40cbb0f
Mark Tassinari <tassinari@gmail.com>
2024-11-11 06:24:49 -0500

    new icon, main view no data page

---
+++ MaynardGolf/MainViewNoData.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewNoData.swift
+//  MaynardGolf
+//
+//  Created by Mark Tassinari on 11/10/24.
+//
+
+import SwiftUI
+
+struct MainViewNoData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainViewNoData()
+}
